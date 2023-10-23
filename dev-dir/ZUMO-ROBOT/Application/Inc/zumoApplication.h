ee6bb20a957bf8f17ea3932ec017b4858788b68e
kierzek <kierzek@student.agh.edu.pl>
2023-10-24 00:37:54 +0200

    Added communication

--- dev-dir/ZUMO-ROBOT/Application/Inc/zumoApplication.h
+++ dev-dir/ZUMO-ROBOT/Application/Inc/zumoApplication.h
@@ -1,4 +1,6 @@
 #include "stm32g4xx_hal.h"
+#include "stm32g4xx_hal_uart.h"
+#include "stm32g4xx_hal_spi.h"
 
 
 void init();
--- dev-dir/ZUMO-ROBOT/Application/Src/zumoApplication.c
+++ dev-dir/ZUMO-ROBOT/Application/Src/zumoApplication.c
@@ -1,7 +1,20 @@
-#include "zumoApplication.h"
+// Include Section
 
+#include "zumoApplication.h"
+#include <stdint.h>
+
+// static vars
+static uint8_t arr[] = {0x00, 0x00, 0x00};
+// external modules
+extern UART_HandleTypeDef huart1;
+extern SPI_HandleTypeDef hspi1;
+// static functions declarations
+// static functions definitions
+// function definitions
 
 void init(void)
 {
+    HAL_UART_Transmit(&huart1, (uint8_t*)&arr, 3, 100);
+    HAL_SPI_Transmit(&hspi1,(uint8_t*)&arr, 3, 100);
     return;
 }
