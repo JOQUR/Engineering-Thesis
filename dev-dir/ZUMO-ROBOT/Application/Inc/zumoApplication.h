7f2cbd8795d6fc781727ec8d7f48c63b6f0d58ab
kierzek <kierzek@student.agh.edu.pl>
2023-10-24 01:03:45 +0200

    Adding FreeRTOS

--- dev-dir/ZUMO-ROBOT/Application/Inc/zumoApplication.h
+++ dev-dir/ZUMO-ROBOT/Application/Inc/zumoApplication.h
@@ -1,6 +1,7 @@
 #include "stm32g4xx_hal.h"
 #include "stm32g4xx_hal_uart.h"
 #include "stm32g4xx_hal_spi.h"
+#include "FreeRTOS.h"
+#include "task.h"
 
 
-void init();
--- dev-dir/ZUMO-ROBOT/Application/Src/zumoApplication.c
+++ dev-dir/ZUMO-ROBOT/Application/Src/zumoApplication.c
@@ -4,7 +4,7 @@
 #include <stdint.h>
 
 // static vars
-static uint8_t arr[] = {0x00, 0x00, 0x00};
+static uint8_t arr[] = {'d', 'u', 'p', 'a'};
 // external modules
 extern UART_HandleTypeDef huart1;
 extern SPI_HandleTypeDef hspi1;
@@ -12,9 +12,23 @@ extern SPI_HandleTypeDef hspi1;
 // static functions definitions
 // function definitions
 
-void init(void)
+static void init(void)
 {
-    HAL_UART_Transmit(&huart1, (uint8_t*)&arr, 3, 100);
-    HAL_SPI_Transmit(&hspi1,(uint8_t*)&arr, 3, 100);
+    uint8_t arrLen = sizeof(arr) / sizeof(arr[0]);
+    HAL_UART_Transmit(&huart1, (uint8_t*)&arr, arrLen, 100);
+    HAL_SPI_Transmit(&hspi1,(uint8_t*)&arr, arrLen, 100);
     return;
 }
+
+
+void taskLED(void* params)
+{
+    // Toggle the LED on pin GPIOG.13
+    init();
+    while (1) {
+        // toggle the LED
+        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_13);
+        // introduce some delay
+        vTaskDelay(500);
+    } // while (1)
+} /* taskLED */ 
